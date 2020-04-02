a0efdbf7091f09cc6dbeea4623baaf1f723e6244
rlin2k1 <rlin2k1@gmail.com>
2020-04-02 13:29:12 -0700

    Implements MAP and REDUCE in wc.go to pass basic mapreduce word count test.

--- src/mapreduce/master.go
+++ src/mapreduce/master.go
@@ -1,14 +1,14 @@
 package mapreduce
 
-import "container/list"
-import "fmt"
-
+import (
+	"container/list"
+	"fmt"
+)
 
 type WorkerInfo struct {
 	address string
 	// You can add definitions here.
 }
-
 
 // Clean up all workers by sending a Shutdown RPC to each one of them Collect
 // the number of jobs each work has performed.
@@ -30,5 +30,30 @@ func (mr *MapReduce) KillWorkers() *list.List {
 
 func (mr *MapReduce) RunMaster() *list.List {
 	// Your code here
+
+	// Read from mr.registerChannel to get NEW WORKERS
+
+	// Only needs to tell the workers Job Number and File Name
+	for i := 0; i < mr.nMap; i++ {
+		workerAddress := <-mr.registerChannel // Name of worker
+
+		args := &DoJobArgs{mr.file, "Map", i, mr.nReduce}
+		var reply DoJobReply
+		go call(workerAddress, "Worker.DoJob", args, reply)
+		_ = reply
+
+		fmt.Printf("REACHED")
+	}
+
+	for i := 0; i < mr.nReduce; i++ {
+		workerAddress := <-mr.registerChannel // Name of worker
+		args := &DoJobArgs{mr.file, "Reduce", i, mr.nMap}
+		var reply DoJobReply
+		go call(workerAddress, "Worker.DoJob", args, reply)
+		_ = reply
+
+	}
+
+	//If Jobs Finish Return
 	return mr.KillWorkers()
 }
